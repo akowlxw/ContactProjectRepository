ab16aa3677523fa53ebd2b61123429ce9216ee64
kovaleva <kovaleva@gmail.com>
2026-03-22 18:28:50 +0300

    Добавлен класс Contact

---
+++ ContactProject/Contact.cs
@@ -0,0 +1,10 @@
+﻿public class Contact
+{
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public Contact(string name, string phoneNumber)
+    {
+        Name = name;
+        PhoneNumber = phoneNumber;
+    }
+}
